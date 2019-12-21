1d787744712962f021aa3dcbc5af85cba48e7d81
Maximilien Galea <m.galea@student.isartdigital.com>
2019-12-21 19:12:38 +0100

    Update gen with room size, need to fix overlaping on same spawn and overlaping size

--- I AM NO NAME/Assets/_IAmNoName/Scripts/LevelGenerator/SpawnRoom.cs
+++ I AM NO NAME/Assets/_IAmNoName/Scripts/LevelGenerator/SpawnRoom.cs
@@ -14,13 +14,16 @@ namespace Com.IsartDigital.IAmNoName.LevelGenerator {
         public event System.Action OnRoomCreated;
 
         private Vector3 distanceFromCenter;
-        Collider[] colliders;
+        private Vector3 roomSize;
+        private Collider[] colliders;
         private void Start() {
             if (LevelGeneration.Instance.stopGeneration) {
                 return;
             }
 
-            colliders = Physics.OverlapSphere(transform.position, 3, RoomMask);
+            roomSize = transform.parent.GetComponent<BoxCollider>().size;
+
+            colliders = Physics.OverlapSphere(transform.position, 0.1f, RoomMask);
 
             if (colliders.Length > 1) {
                 Destroy(gameObject);
@@ -31,40 +34,64 @@ namespace Com.IsartDigital.IAmNoName.LevelGenerator {
 
 
             distanceFromCenter = transform.position - transform.parent.position;
-            Debug.Log(distanceFromCenter);
+            //Debug.Log(distanceFromCenter);
             Invoke("CreateNewRoom", 0.1f);
             //CreateNewRoom();
         }
 
         private void CreateNewRoom() {
             GameObject nextRoom = null;
-
+            Vector3 nextRoomPos = Vector3.zero;
+            Vector3 nextRoomSize = Vector3.zero;
             // create right or left
             if (Mathf.Abs(distanceFromCenter.x) > Mathf.Abs(distanceFromCenter.y)) {
                 if (distanceFromCenter.x > 0) {
                     int rand = Random.Range(0, LevelGeneration.Instance.leftRooms.Length);
                     nextRoom = Instantiate(LevelGeneration.Instance.leftRooms[rand], transform.position, Quaternion.identity);
+
+                    nextRoomSize = nextRoom.GetComponent<BoxCollider>().size;
+
+                    nextRoomPos = transform.parent.position + new Vector3(roomSize.x / 2 + nextRoomSize.x / 2, 0, 0);
+
+                    nextRoom.transform.position = nextRoomPos;
                 }
                 // room with rigth open
                 else {
                     int rand = Random.Range(0, LevelGeneration.Instance.rightRooms.Length);
                     nextRoom = Instantiate(LevelGeneration.Instance.rightRooms[rand], transform.position, Quaternion.identity);
+
+                    nextRoomSize = nextRoom.GetComponent<BoxCollider>().size;
+
+                    nextRoomPos = transform.parent.position - new Vector3(roomSize.x / 2 + nextRoomSize.x / 2, 0, 0);
+
+
+                    nextRoom.transform.position = nextRoomPos;
                 }
                 // create up or down
             } else {
                 if (distanceFromCenter.y > 0) {
                     int rand = Random.Range(0, LevelGeneration.Instance.bottomRooms.Length);
                     nextRoom = Instantiate(LevelGeneration.Instance.bottomRooms[rand], transform.position, Quaternion.identity);
+
+                    nextRoomSize = nextRoom.GetComponent<BoxCollider>().size;
+
+                    nextRoomPos = transform.parent.position + new Vector3(0, roomSize.y / 2 + nextRoomSize.y / 2, 0);
+
+                    nextRoom.transform.position = nextRoomPos;
                 }
                 // room with rigth open
                 else {
                     int rand = Random.Range(0, LevelGeneration.Instance.topRooms.Length);
                     nextRoom = Instantiate(LevelGeneration.Instance.topRooms[rand], transform.position, Quaternion.identity);
+
+                    nextRoomSize = nextRoom.GetComponent<BoxCollider>().size;
+
+                    nextRoomPos = transform.parent.position - new Vector3(0, roomSize.y / 2 + nextRoomSize.y / 2, 0);
+
+                    nextRoom.transform.position = nextRoomPos;
                 }
             }
 
-            Vector3 nextRoomPos = nextRoom.transform.position - transform.parent.position;
-            nextRoom.transform.position += nextRoomPos;
             OnRoomCreated?.Invoke();
             Destroy(gameObject);
         }
@@ -73,10 +100,5 @@ namespace Com.IsartDigital.IAmNoName.LevelGenerator {
             //OnRoomCreated = null;
         }
 
-        private void OnTriggerEnter(Collider other) {
-            if (other.CompareTag("Room")) {
-                Destroy(gameObject);
-            }
-        }
     }
 }
